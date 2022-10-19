8716add682b269cc21374bf902d90e4cc74c24e2
hhdk0 <105486016+hhdk0@users.noreply.github.com>
2022-10-19 09:02:23 +0900

    221019 오전커밋

---
+++ src/main/java/com/kh/mobiil/space/controller/SpaceController.java
@@ -0,0 +1,13 @@
+package com.kh.mobiil.space.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.kh.mobiil.space.service.SpaceService;
+
+@Controller
+public class SpaceController {
+	
+	@Autowired
+	private SpaceService sService;
+}
---
+++ src/main/java/com/kh/mobiil/space/service/SpaceService.java
@@ -0,0 +1,5 @@
+package com.kh.mobiil.space.service;
+
+public class SpaceService {
+
+}
