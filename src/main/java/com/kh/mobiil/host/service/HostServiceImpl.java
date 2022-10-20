677f3e400f6b1280fe0e01be4ecad8f51eff1aaf
chj <gpwls@DESKTOP-7VVNA2Q>
2022-10-20 09:37:18 +0900

    20221020 머지&푸시

--- src/main/java/com/kh/mobiil/host/service/HostServiceImpl.java
+++ src/main/java/com/kh/mobiil/host/service/HostServiceImpl.java
@@ -1,102 +1,102 @@
-package com.kh.mobiil.host.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-import regervation.Reservation;
-
-import com.kh.mobiil.host.domain.Host;
-import com.kh.mobiil.host.store.HostStore;
-import com.kh.mobiil.space.domain.Space;
-import com.kh.mobiil.space.domain.SpaceImg;
-
-@Service
-public class HostServiceImpl implements HostService{
-
-	@Autowired
-	private HostStore hStore;
-	@Autowired
-	private SqlSession session;
-	
-	@Override
-	public Host getHostInfo(String hostEmail) {
-		Host host = hStore.getHostInfo(session, hostEmail);
-		return host;
-	}
-
-	@Override
-	public int hostModify(Host host) {
-		int result = hStore.hostModify(session, host);
-		return result;
-	}
-
-	@Override
-	public int getRegervationTotalCount() {
-		int result = hStore.getRegervationTotalCount(session);
-		return result;
-	}
-
-	@Override
-	public List<Reservation> regervationList(int currentPage, int limit) {
-		List<Reservation> rList = hStore.regervationList(session, currentPage, limit);
-		return rList;
-	}
-	
-	@Override
-	public int insertSpace(Space space) {
-		int result = hStore.insertSpace(session, space);
-		return result;
-	}
-
-	@Override
-	public int insertSpaceImg(SpaceImg spaceImg) {
-		int result = hStore.insertSpaceImg(session, spaceImg);
-		return result;
-	}
-
-	@Override
-	public int getSpaceTotalCount() {
-		int result = hStore.getSpaceTotalCount(session);
-		return result;
-	}
-
-	@Override
-	public List<Space> spaceList(int currentPage, int boardLimit) {
-		List<Space> sList = hStore.spaceList(session, currentPage, boardLimit);
-		return sList;
-	}
-
-	@Override
-	public Space spaceByNo(Integer spaceNo) {
-		Space space = hStore.spaceByNo(session, spaceNo);
-		return space;
-	}
-
-	@Override
-	public List<SpaceImg> spaceImgByNo(Integer spaceNo) {
-		List<SpaceImg> sImg = hStore.spaceImgByNo(session, spaceNo);
-		return sImg;
-	}
-
-	@Override
-	public int spaceModify(Space space) {
-		int result = hStore.spaceModify(session, space);
-		return result;
-	}
-
-	@Override
-	public int spaceImgModify(SpaceImg spaceImg) {
-		int result = hStore.spaceImgModify(session, spaceImg);
-		return result;
-	}
-
-	@Override
-	public int spaceRemove(Integer spaceNo) {
-		int result = hStore.spaceRemove(session, spaceNo);
-		return result;
-	}
-
-}
+//package com.kh.mobiil.host.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.session.SqlSession;
+//import regervation.Reservation;
+//
+//import com.kh.mobiil.host.domain.Host;
+//import com.kh.mobiil.host.store.HostStore;
+//import com.kh.mobiil.space.domain.Space;
+//import com.kh.mobiil.space.domain.SpaceImg;
+//
+//@Service
+//public class HostServiceImpl implements HostService{
+//
+//	@Autowired
+//	private HostStore hStore;
+//	@Autowired
+//	private SqlSession session;
+//	
+//	@Override
+//	public Host getHostInfo(String hostEmail) {
+//		Host host = hStore.getHostInfo(session, hostEmail);
+//		return host;
+//	}
+//
+//	@Override
+//	public int hostModify(Host host) {
+//		int result = hStore.hostModify(session, host);
+//		return result;
+//	}
+//
+//	@Override
+//	public int getRegervationTotalCount() {
+//		int result = hStore.getRegervationTotalCount(session);
+//		return result;
+//	}
+//
+//	@Override
+//	public List<Reservation> regervationList(int currentPage, int limit) {
+//		List<Reservation> rList = hStore.regervationList(session, currentPage, limit);
+//		return rList;
+//	}
+//	
+//	@Override
+//	public int insertSpace(Space space) {
+//		int result = hStore.insertSpace(session, space);
+//		return result;
+//	}
+//
+//	@Override
+//	public int insertSpaceImg(SpaceImg spaceImg) {
+//		int result = hStore.insertSpaceImg(session, spaceImg);
+//		return result;
+//	}
+//
+//	@Override
+//	public int getSpaceTotalCount() {
+//		int result = hStore.getSpaceTotalCount(session);
+//		return result;
+//	}
+//
+//	@Override
+//	public List<Space> spaceList(int currentPage, int boardLimit) {
+//		List<Space> sList = hStore.spaceList(session, currentPage, boardLimit);
+//		return sList;
+//	}
+//
+//	@Override
+//	public Space spaceByNo(Integer spaceNo) {
+//		Space space = hStore.spaceByNo(session, spaceNo);
+//		return space;
+//	}
+//
+//	@Override
+//	public List<SpaceImg> spaceImgByNo(Integer spaceNo) {
+//		List<SpaceImg> sImg = hStore.spaceImgByNo(session, spaceNo);
+//		return sImg;
+//	}
+//
+//	@Override
+//	public int spaceModify(Space space) {
+//		int result = hStore.spaceModify(session, space);
+//		return result;
+//	}
+//
+//	@Override
+//	public int spaceImgModify(SpaceImg spaceImg) {
+//		int result = hStore.spaceImgModify(session, spaceImg);
+//		return result;
+//	}
+//
+//	@Override
+//	public int spaceRemove(Integer spaceNo) {
+//		int result = hStore.spaceRemove(session, spaceNo);
+//		return result;
+//	}
+//
+//}
--- src/main/java/com/kh/mobiil/host/store/HostStore.java
+++ src/main/java/com/kh/mobiil/host/store/HostStore.java
@@ -1,41 +1,41 @@
-package com.kh.mobiil.host.store;
-
-import java.util.List;
-
-import org.apache.ibatis.session.SqlSession;
-
-import com.kh.mobiil.host.domain.Host;
-import com.kh.mobiil.space.domain.Space;
-import com.kh.mobiil.space.domain.SpaceImg;
-
-import regervation.Reservation;
-
-
-public interface HostStore {
-
-	Host getHostInfo(SqlSession session, String hostEmail);
-	
-	int hostModify(SqlSession session, Host host);
-
-	int getRegervationTotalCount(SqlSession session);
-
-	List<Reservation> regervationList(SqlSession session, int currentPage, int limit);
-	
-	int insertSpace(SqlSession session, Space space);
-
-	int insertSpaceImg(SqlSession session, SpaceImg spaceImg);
-
-	int getSpaceTotalCount(SqlSession session);
-
-	List<Space> spaceList(SqlSession session, int currentPage, int boardLimit);
-
-	Space spaceByNo(SqlSession session, Integer spaceNo);
-
-	List<SpaceImg> spaceImgByNo(SqlSession session, Integer spaceNo);
-
-	int spaceModify(SqlSession session, Space space);
-
-	int spaceImgModify(SqlSession session, SpaceImg spaceImg);
-
-	int spaceRemove(SqlSession session, Integer spaceNo);
-}
+//package com.kh.mobiil.host.store;
+//
+//import java.util.List;
+//
+//import org.apache.ibatis.session.SqlSession;
+//
+//import com.kh.mobiil.host.domain.Host;
+//import com.kh.mobiil.space.domain.Space;
+//import com.kh.mobiil.space.domain.SpaceImg;
+//
+//import regervation.Reservation;
+//
+//
+//public interface HostStore {
+//
+//	Host getHostInfo(SqlSession session, String hostEmail);
+//	
+//	int hostModify(SqlSession session, Host host);
+//
+//	int getRegervationTotalCount(SqlSession session);
+//
+//	List<Reservation> regervationList(SqlSession session, int currentPage, int limit);
+//	
+//	int insertSpace(SqlSession session, Space space);
+//
+//	int insertSpaceImg(SqlSession session, SpaceImg spaceImg);
+//
+//	int getSpaceTotalCount(SqlSession session);
+//
+//	List<Space> spaceList(SqlSession session, int currentPage, int boardLimit);
+//
+//	Space spaceByNo(SqlSession session, Integer spaceNo);
+//
+//	List<SpaceImg> spaceImgByNo(SqlSession session, Integer spaceNo);
+//
+//	int spaceModify(SqlSession session, Space space);
+//
+//	int spaceImgModify(SqlSession session, SpaceImg spaceImg);
+//
+//	int spaceRemove(SqlSession session, Integer spaceNo);
+//}
